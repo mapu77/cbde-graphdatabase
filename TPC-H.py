8e2cd5613bc0edb11a4537de13409f64b506f49f
Eduard Maura <edu.maura.puig@gmail.com>
2016-11-24 17:32:14 +0100

    Adapt to Python 3.5 and add db configuration

--- TPC-H.py
+++ TPC-H.py
@@ -1,31 +1,35 @@
 import datetime
 
+from neo4j.v1 import GraphDatabase, basic_auth
+
+from db_configuration import get_password
 from inserts import insert_sample_data
 from queries import execute_query_1, execute_query_2, execute_query_3, execute_query_4
 
 
 def show_options():
     print("Enter an option to do:")
-    print ("\t 1 - Execute Query 1")
-    print ("\t 2 - Execute Query 2")
-    print ("\t 3 - Execute Query 3")
-    print ("\t 4 - Execute Query 4")
-    print ("\t 5 - Inserts (Warning: this can only be done once)")
-    print ("\t 0 - Exit")
+    print("\t 1 - Execute Query 1")
+    print("\t 2 - Execute Query 2")
+    print("\t 3 - Execute Query 3")
+    print("\t 4 - Execute Query 4")
+    print("\t 5 - Inserts (Warning: this can only be done once)")
+    print("\t 0 - Exit")
 
 
 # Start of the program
-db = None
+driver = GraphDatabase.driver("bolt://localhost", auth=basic_auth("neo4j", get_password()))
+db = driver.session()
 
 show_options()
-op = input()
+op = int(input())
 
 while op != 0:
     if op == 1:
         date = str(input("Please enter a date: (yyyy-mm-dd) "))
         execute_query_1(db, datetime.datetime.strptime(date, "%Y-%m-%d"))
     elif op == 2:
-        size = input("Please enter a size: ")
+        size = int(input("Please enter a size: "))
         types = input("Please enter a type: ")
         region = input("Please enter a region: ")
         execute_query_2(db, size, types, region)
@@ -33,19 +37,20 @@ while op != 0:
         segment = input("Please enter a segment: ")
         date1 = input("Please enter the first date: ")
         date2 = input("Please enter the second date: ")
-        execute_query_3(db, segment, datetime.datetime.strptime(date1, "%Y-%m-%d"),datetime.datetime.strptime(date2, "%Y-%m-%d"))
+        execute_query_3(db, segment, datetime.datetime.strptime(date1, "%Y-%m-%d"),
+                        datetime.datetime.strptime(date2, "%Y-%m-%d"))
     elif op == 4:
         name = input("Please enter a region: ")
         date = input("Please enter a date: ")
-        execute_query_4(db,name, datetime.datetime.strptime(date, "%Y-%m-%d"))
+        execute_query_4(db, name, datetime.datetime.strptime(date, "%Y-%m-%d"))
     elif op == 5:
-        print ("Inserting sample data...")
+        print("Inserting sample data...")
         insert_sample_data(db)
-        print ("Done!")
+        print("Done!")
     else:
-        print ("Error: This option is not valid")
+        print("Error: This option is not valid")
 
     show_options()
-    op = input()
+    op = int(input())
 
-print ("Bye!")
+print("Bye!")
---
+++ db_configuration.py
@@ -0,0 +1,3 @@
+def get_password():
+    raise Exception("Delete this line and return your password")
+    return ""
