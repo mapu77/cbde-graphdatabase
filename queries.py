1c6724f9c68e28c8f53e4d4de9545eff6f6d60e5
Eduard Maura <edu.maura.puig@gmail.com>
2016-11-30 00:49:47 +0100

    Complete Query 2

--- queries.py
+++ queries.py
@@ -1,4 +1,3 @@
-
 def execute_query_1(db, date):
     result = db.run("MATCH (n:LineItem) "
                     "WHERE n.l_shipdate <= {date}"
@@ -11,9 +10,23 @@ def execute_query_1(db, date):
 
     for record in result:
         print(record)
+    print()
 
-def execute_query_2(db, size, types, region):
-    pass
+
+def execute_query_2(db, size, type, region):
+    result = db.run(
+        "MATCH (s:Supplier)-[S:SUPPLIES]->() where s.r_name = {region} return min(S.ps_supplycost) as min_supplycost",
+        {"region": region})
+    for record in result:
+        min_supplycost = float(record["min_supplycost"])
+
+    result = db.run("MATCH (p:Part)<-[S:SUPPLIES]-(s:Supplier) "
+                    "WHERE p.p_size = 1 and p.p_type STARTS WITH 'Ba' and s.r_name = 'BCN' and S.ps_supplycost = 1.98 "
+                    "RETURN s.s_acctbal, s.s_name, s.r_name, p.p_partkey, p.p_mfgr, s.s_address, s.s_phone, s.s_comment",
+                    {"size": size, "type": type, "region": region})
+    for record in result:
+        print(record)
+    print()
 
 
 def execute_query_3(db, segment, date1, date2):
